ca2022bf3b073ef22f4f91dcfbc0026174c3fd1a
cruble <cruble4@gmail.com>
2015-05-12 18:19:22 -0400

    removes old comments

--- timecoder.py
+++ timecoder.py
@@ -8,17 +8,7 @@ class StarttimeCommand(sublime_plugin.TextCommand):
         start_time = datetime.datetime.now()
         
         start_time_stamp = "// Start Time: %s\t // Timer: 00:00:00\n" % (start_time.strftime("%H:%M:%S"))
-        # self.view.insert(edit, self.view.sel()[0].begin(), timestamp)
         self.view.insert(edit, 0, start_time_stamp)
-
-        # try:
-        #     # line = int(text)
-        #     if self.window.active_view():
-        #         self.window.active_view().run_command("timecoder", {"start_time": start_time} )
-        # except ValueError:
-        #     pass
-
-    #     self.view.window().run_command("timecoder", {"start_time": start_time} )
         self.view.run_command(self.view.id, "timecoder", {"start_time": start_time })
     pass
 
@@ -37,34 +27,3 @@ class TimecoderCommand(sublime_plugin.TextCommand):
         self.view.insert(edit, self.view.sel()[0].begin(), timestamp)
 
         pass
-
-
-# class TimecoderCommand(sublime_plugin.TextCommand):
-#   def run(self, edit):
-#     val = time().strftime('%Y-%M-%d')
-#     self.view.insert(edit, self.view.sel()[0].begin(), val)
-
-
-        # print '%s:%s:%s' % (hours, minutes, seconds)
-        # difference = 
-        # timestamp = "-%s-\t" % (datetime.datetime.now().strftime("%H:%M:%S"))
-        # timestamp = "-%s-\t" % (elapsed_time.strftime("%H:%M:%S"))
-
-
-# class TimecoderCommand(sublime_plugin.EventListener):
-#     """Expand `isoD`, `now`, `datetime`, `utcnow`, `utcdatetime`, 
-#        `date` and `time`
-#     """
-#     def on_query_completions(self, view, prefix, locations):
-#         if prefix in ('isoD', 'now', 'datetime'):
-#             val = datetime.now().strftime('%Y-%M-%dT%H:%M:%S')
-#         elif prefix in ('utcnow', 'utcdatetime'):
-#             val = datetime.utcnow().strftime('%Y-%M-%dT%H:%M:%S')
-#         elif prefix == 'date':
-#             val = datetime.now().strftime('%Y-%M-%d')
-#         elif prefix == 'time':
-#             val = datetime.now().strftime('%H:%M:%S')
-#         else:
-#             val = None
- 
-#         return [(prefix, prefix, val)] if val else []
